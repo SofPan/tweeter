bb278793b6392fef1e00eaca352ad5e054275b99
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-12-04 11:17:25 -0500

    implement renderTweets function and use it to append createTweetElement function's return value to the tweets-container page element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,48 @@
 /* eslint-disable no-undef */
 
 // Dummy data
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 $(document).ready(() => {
+
+  const renderTweets = (tweets) => {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $createdTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($createdTweet);
+    }
+  };
 
   const createTweetElement = (tweet) => {
     const user = tweet.user;
     const content = tweet.content;
+    // Get day month & year from date string
     const date = new Date(tweet.created_at).toDateString().split(" ").slice(1).join(" ");
 
     return `<article class="tweet">
@@ -40,7 +65,6 @@ $(document).ready(() => {
       </article>`;
   };
 
-  const $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
 
