f8dd441f331f3cd5618daed9693d3f0f70b669ee
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-12-06 18:00:07 -0500

    fix issue where counter would not adjust on backspace or if user is pasting in text

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-undef */
 $(document).ready(() => {
-  $("#tweet-text").keypress(function (e) {
-    const charactersTyped = $(this).val();
+  $("#tweet-text").on("input", function (e) {
     const counter = $(this).parent().find('.counter');
-    const charactersRemaining = 140 - (charactersTyped.length + 1);
+    const charactersRemaining = 140 - (this.value.length + 1);
     counter.text(charactersRemaining);
     if (charactersRemaining <= 0) {
       counter.addClass('text-alert');
