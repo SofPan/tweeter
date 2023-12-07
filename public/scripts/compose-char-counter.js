ab5a62a976c6fa12701c22fa24733a2a9ba32b2b
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-12-07 18:43:30 -0500

    fix bug where counter decrements by 2 at the start

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   $("#tweet-text").on("input", function (e) {
     const counter = $(this).parent().find('.counter');
-    const charactersRemaining = 140 - (this.value.length + 1);
+    const charactersRemaining = 140 - (this.value.length);
     counter.text(charactersRemaining);
     if (charactersRemaining <= 0) {
       counter.addClass('text-alert');
